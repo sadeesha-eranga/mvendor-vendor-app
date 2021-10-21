0e4ef23b388aea3e2249231665bf1cddb1a3d9b8
Sadeesha_Zincat <sadeesha.eranga@dialog.lk>
2021-10-21 23:07:52 +0530

    Add route on route map

--- components/OnRouteMap.js
+++ components/OnRouteMap.js
@@ -4,10 +4,17 @@ import tw from "tailwind-react-native-classnames";
 import { Image } from 'react-native';
 import http from '../utils/http';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import MapViewDirections from 'react-native-maps-directions';
+import {GOOGLE_MAPS_APIKEY} from "@env";
 
 function OnRouteMap(props) {
+
   const [initialRegion, setInitialRegion] = useState(null);
   const [customers, setCustomers] = useState([]);
+  const [mapRef, setMapRef] = useState(null);
+  const [coords, setCoords] = useState([]);
+  const [startLatLon, setStartLatLon] = useState({latitude: 0, longitude: 0});
+  const [endLatLon, setEndLatLon] = useState({latitude: 0, longitude: 0});
 
   const updateAwaitingCustomers = async () => {
     try {
@@ -35,6 +42,10 @@ function OnRouteMap(props) {
       }
     })();
 
+    setCoords(props.item.locationPoints);
+    setStartLatLon({latitude: props.item.startingLatitude, longitude: props.item.startingLongitude});
+    setEndLatLon({latitude: props.item.endingLatitude, longitude: props.item.endingLongitude});
+
     const timer = setInterval(() => {
       updateAwaitingCustomers().then();
     }, 5000);
@@ -50,8 +61,39 @@ function OnRouteMap(props) {
       style={tw`flex-1`}
       showsUserLocation={true}
       initialRegion={initialRegion}
+      ref={(ref) => {
+        setMapRef(ref);
+      }}
+      onLayout={() => mapRef.fitToCoordinates(coords, {
+        edgePadding: {top: 10, right: 10, bottom: 10, left: 10},
+        animated: true
+      })}
     >
-      {customers.map(customer => (<Marker
+      {coords.length > 2 && <MapViewDirections
+        lineDashPattern={[1]}
+        origin={startLatLon}
+        destination={endLatLon}
+        waypoints={(coords.length > 2) ? coords : null}
+        apikey={GOOGLE_MAPS_APIKEY}
+        strokeWidth={4}
+        strokeColor="#E6AD00"
+        optimizeWaypoints={false}
+        mode={'DRIVING'}
+        onReady={result => {
+          console.log(`Distance: ${result.distance} km`)
+          console.log(`Duration: ${result.duration} min.`)
+
+          mapRef.fitToCoordinates(result.coordinates, {
+            edgePadding: {top: 10, right: 10, bottom: 10, left: 10},
+            animated: true
+          });
+        }}
+        onError={(errorMessage) => {
+          console.log('GOT AN ERROR', errorMessage);
+        }}
+      />}
+      {customers.map((customer, index) => (<Marker
+        key={index}
         coordinate={{
           latitude: customer.latitude,
           longitude: customer.longitude
